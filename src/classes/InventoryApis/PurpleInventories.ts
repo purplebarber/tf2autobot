bda45bfd3f52ebac2bdea46793e5bb381db8af36
Purple <87976511+purplebarber@users.noreply.github.com>
2023-08-30 18:23:31 +0800

    Create PurpleInventories.ts

---
+++ src/classes/InventoryApis/PurpleInventories.ts
@@ -0,0 +1,27 @@
+import { UnknownDictionaryKnownValues } from 'src/types/common';
+import Bot from '../Bot';
+import InventoryApi from './InventoryApi';
+
+export default class PurpleInventories extends InventoryApi {
+    private readonly pricerApiKey: string;
+
+    private readonly pricerUrl: string;
+
+    constructor(bot: Bot) {
+        super(bot, 'purpleInventories');
+        const pricerSettings = bot.priceSource.getOptions();
+        this.pricerApiKey = pricerSettings.pricerApiToken;
+        this.pricerUrl = pricerSettings.pricerUrl;
+    }
+
+    protected getURLAndParams(
+        steamID64: string,
+        appID: number,
+        contextID: string
+    ): [string, UnknownDictionaryKnownValues] {
+        return [
+          `${this.pricerUrl}/inventory/${steamID64}/${appID}/${contextID}`,
+            { token: this.pricerApiKey }
+        ];
+    }
+}
