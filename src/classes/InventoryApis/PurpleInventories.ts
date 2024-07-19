97217121479371c4b96373be5c7156cb0f6c1821
Purple <87976511+purplebarber@users.noreply.github.com>
2024-07-20 02:08:37 +0400

    Update PurpleInventories.ts

--- src/classes/InventoryApis/PurpleInventories.ts
+++ src/classes/InventoryApis/PurpleInventories.ts
@@ -20,7 +20,7 @@ export default class PurpleInventories extends InventoryApi {
         contextID: string
     ): [string, UnknownDictionaryKnownValues] {
         return [
-          `${this.pricerUrl}/inventory/${steamID64}/${appID}/${contextID}/?token=${this.pricerApiKey}`,
+          `${this.pricerUrl}/inventory/${steamID64}/${appID}/${contextID}?token=${encodeURIComponent(this.pricerApiKey)}`,
             {steamID64, appID, contextID, token: this.pricerApiKey}
         ];
     }
